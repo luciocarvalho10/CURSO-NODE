a76dee1f0aa0da244cd2bf4fb213fd3e476a814d
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2022-02-12 15:27:58 -0400

    melhorando modulo path

--- modules/path.js
+++ modules/path.js
@@ -1,16 +1,28 @@
 const path = require('path')
 
 // Pegando o nome do arquivo atual
-console.log(path.basename(__filename))
+const arquivoAtual= () => console.log(path.basename(__filename))
 
 // Pegando Diretório atual
-console.log(path.dirname(__filename))
+const arquivoAtualDiretorio = () => console.log(path.dirname(__filename))
 
 // Pegando extensão do arquivo atual
-console.log(path.extname(__filename))
+const arquivoAtualExtensao = () => console.log(path.extname(__filename))
 
 // Pegando objeto de informação do arquivo atual
-console.log(path.parse(__filename))
+const arquivoAtualInformacao = () => console.log(path.parse(__filename))
 
 // Juntar caminhos
-console.log(path.join(__dirname, 'teste'))
+const arquivoAtualJuntarCaminho = caminho => console.log(path.join(__dirname, caminho))
+
+// Juntar caminho pode por arquivos também
+// const arquivoCaminhoJuntar = () =>
+//   console.log(path.join(__dirname, 'teste', 'teste.html'))
+
+module.exports = {
+  arquivoAtual,
+  arquivoAtualDiretorio,
+  arquivoAtualExtensao,
+  arquivoAtualInformacao,
+  arquivoAtualJuntarCaminho
+}
