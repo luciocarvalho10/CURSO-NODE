05ead017b2bfb2f0773b5248615edaa12a07b196
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2022-02-12 18:16:43 -0400

    corrigindo msg para succ

--- modules/msg.js
+++ modules/msg.js
@@ -1,5 +1,5 @@
 module.exports = {
-  msg: msg => console.log(msg),
+  succ: msg => console.log(msg),
   err: err => console.log('Error: ' + err),
   errSucc: (err, msg) => (err ? console.log('Error: ' + err) : console.log(msg))
 }
