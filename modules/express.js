6d70d6e13f25498c7c7f8a287cbd626c493addbe
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2022-02-19 12:34:20 -0400

    criando post e editando get

--- modules/express.js
+++ modules/express.js
@@ -1,27 +1,25 @@
 const express = require('express')
 const msg = require('./msg')
+const UserModel = require('../src/models/user.model')
 
 const port = 8080
 const app = express()
 
-app.get('/home', (req,res) => {
-  res.contentType('apllication/html')
-  res.status(200).send('<h1>Hello World!</h1>')
+app.use(express.json())
+
+app.get('/users', async (req, res) => {
+  try {
+    res.status(200).json(await UserModel.find({}))
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
 })
 
-app.get('/users', (req, res) => {
-  const usersResp = [
-    {
-      name: 'Lucio',
-      email: 'lucio@teste.com'
-    },
-    {
-      name: 'Lucio2',
-      email: 'lucio2@teste.com'
-    }
-  ]
-
-  res.status(200).send(usersResp)
-})
+app.post('/users', async (req,res) => {
+    try {
+      res.status(201).json(await UserModel.create(req.body))
+    } catch (error) {
+      res.status(500).send(error.message)
+  }})
 
 app.listen(port, () => msg.succ(`Rodando na porta ${port}`))
