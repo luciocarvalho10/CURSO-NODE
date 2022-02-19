ca25eda40d643dc20cd702380f12e7cdf8853457
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2022-02-19 19:03:35 -0400

    criando atualizar com patch

--- modules/express.js
+++ modules/express.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json())
 
 // pegar todos os usuarios
-app.get('/users', async (req, res) => {
+app.get('/users', async (_req, res) => {
   try { res.status(200).json(await UserModel.find({})) }
   catch (error) { res.status(500).send(error.message) }
 })
@@ -25,4 +25,10 @@ app.post('/users', async (req,res) => {
     catch (error) { res.status(500).send(error.message) }
 })
 
-app.listen(port, () => msg.succ(`Rodando na porta ${port}`))
+// atualizar usuario
+app.patch('/users/:id', async (req, res) => {
+  try {res.status(200).json(await UserModel.findByIdAndUpdate(req.params.id, req.body, {new: true}))}
+  catch (error) { res.status(500).send(error.message) }
+})
+
+app.listen(port, () => msg.succ(`Rodando na porta ${port} - ${new Date().toLocaleString()}`))
--- src/database/connect.js
+++ src/database/connect.js
@@ -1,6 +1,8 @@
 const msg = require('../../modules/msg')
 const mongoose = require('mongoose')
 
+mongoose.Promise = global.Promise
+
 const connectDatabase = async () => {
   await mongoose.connect(
     `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cursonodejs.rtalm.mongodb.net/database?retryWrites=true&w=majority`,
