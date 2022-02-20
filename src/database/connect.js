01a6af10a567b795ea82cc3876fde7888d1aa83d
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2022-02-19 20:44:27 -0400

    acrescentando data no log do banco

--- src/database/connect.js
+++ src/database/connect.js
@@ -6,7 +6,7 @@ mongoose.Promise = global.Promise
 const connectDatabase = async () => {
   await mongoose.connect(
     `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cursonodejs.rtalm.mongodb.net/database?retryWrites=true&w=majority`,
-    e => msg.errSucc(e, 'cloud.mongodb.com conectado!')
+    e => msg.errSucc(e, `cloud.mongodb.com conectado! - ${new Date().toLocaleString()}`)
   )
 }
 
