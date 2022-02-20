b127b601983b0c89a92327e328ffaff8af272759
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2022-02-19 20:44:50 -0400

    craindo deletar usuario

--- modules/express.js
+++ modules/express.js
@@ -31,4 +31,9 @@ app.patch('/users/:id', async (req, res) => {
   catch (error) { res.status(500).send(error.message) }
 })
 
+app.delete('/users/:id', async (req, res) => {
+  try { res.status(200).json(await UserModel.findByIdAndRemove(req.params.id))}
+  catch (error) { res.status(500).send(error.message) }
+})
+
 app.listen(port, () => msg.succ(`Rodando na porta ${port} - ${new Date().toLocaleString()}`))
